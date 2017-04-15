a52856645997193a4f0de3cd7ba9f0a29a53c93a
Hrishabh Gupta <hrishabhg@gmail.com>
2017-04-15 13:26:40 +0530

    Test

--- cw_logging/__init__.py
+++ cw_logging/__init__.py
@@ -1,9 +1,12 @@
+from django.conf import settings
 import logging
 
 __author__ = 'Hrishabh Gupta<hrishabhg@gmail.com>'
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 
 
 class CloudWatchHandler(logging.Handler):
     def emit(self, record):
+        print record
+        print getattr(settings, 'CLOUDWATCH_AWS_ACCESS_KEY', '---')
         pass
